afdcbad3d1eba59a2956a389b5555b095d92535c
b08501113 <b08501113@ntu.edu.tw>
2023-12-19 21:09:15 +0800

    1219_2100

--- inference.sh
+++ inference.sh
@@ -12,5 +12,12 @@
 
 module load opt gcc python/3.9.13-gpu
 sbatch_pre.sh
-bash run.sh Taiwan-LLM-7B-v2.0-chat/ axolotl/qlora-out_final/ data/final/test_one.json prediction.json combine.json
+bash run.sh Taiwan-LLM-7B-v2.0-chat/ axolotl/qlora-out_final_3000_one/ data/final/test_one.json prediction.json 3000_lora_1.json
+bash run.sh Taiwan-LLM-7B-v2.0-chat/ axolotl/qlora-out_final_3000_two/ data/final/test_two.json prediction.json 3000_lora_2.json
+bash run.sh Taiwan-LLM-7B-v2.0-chat/ axolotl/qlora-out_final_5000_zero/ data/final/test_zero.json prediction.json 5000_lora_0.json
+bash run.sh Taiwan-LLM-7B-v2.0-chat/ axolotl/qlora-out_final_5000_one/ data/final/test_one.json prediction.json 5000_lora_1.json
+bash run.sh Taiwan-LLM-7B-v2.0-chat/ axolotl/qlora-out_final_5000_two/ data/final/test_two.json prediction.json 5000_lora_2.json
+bash run.sh Taiwan-LLM-7B-v2.0-chat/ axolotl/qlora-out_final_10000_zero/ data/final/test_zero.json prediction.json 10000_lora_0.json
+bash run.sh Taiwan-LLM-7B-v2.0-chat/ axolotl/qlora-out_final_10000_one/ data/final/test_one.json prediction.json 10000_lora_1.json
+bash run.sh Taiwan-LLM-7B-v2.0-chat/ axolotl/qlora-out_final_10000_two/ data/final/test_two.json prediction.json 10000_lora_2.json
 sbatch_post.sh
